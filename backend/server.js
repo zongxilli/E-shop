deb37449776190bb6c51d54286598906d13d6173
Zongxi Li <zongxi2014@gmail.com>
2021-06-23 01:18:58 -0400

    Created new productRoute to & frontend to backend back to be connected

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import  express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 // Remember to keep .js at the end
-import products from './data/products.js';
 import connectDB from './config/db.js';
 
-// Run both frontend & backend command: npm run dev
+import productRoutes from './routes/productRoutes.js';
+
+
 
 dotenv.config();
 
@@ -19,16 +20,7 @@ app.get('/', (req, res) => {
 	res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find(
-		(eachProduct) => eachProduct._id === req.params.id
-	);
-	res.json(product);
-});
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 
@@ -38,3 +30,6 @@ app.listen(
 		`Server Running In < ${process.env.NODE_ENV} mode > On Port: ${PORT}`.cyan.underline
 	)
 );
+
+
+// Run both frontend & backend command: npm run dev
