ff4f460bd8e364f1abc3c569bcb3baa74d96f809
Zongxi Li <zongxi2014@gmail.com>
2021-06-22 21:07:56 -0400

    Connected frontend to backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+// We putted "type": "module" in package.json to be able to use ES15 syntax
+// And also use node version later than 12
+import  express from 'express';
+import dotenv from 'dotenv';
+// Remember to keep .js at the end
+import products from './data/products.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find(
+		(eachProduct) => eachProduct._id === req.params.id
+	);
+	res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(
+		`Server Running In << ${process.env.NODE_ENV} mode >> On Port --- ${PORT}`
+	)
+);
