ed5812263d16cd25fdd10b65eb826b7c340f09f0
zongxilli <zongxi2014@gmail.com>
2021-07-08 14:44:39 -0400

    add logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { logout } from '../actions/userActions';
 
-const Header = () => {
+const Header = ({ location, history }) => {
+	const dispatch = useDispatch();
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const logoutHandler = () => {
+		dispatch(logout());
+		//todo Me Added redirect to the signIn page after people signing out using withRouter
+		history.push("/login");
+	};
+
 	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" CollapseOnSelect>
@@ -19,13 +32,23 @@ const Header = () => {
 									<i className="fas fa-shopping-cart"></i> Cart
 								</Nav.Link>
 							</LinkContainer>
-
-							{/* -------------------- login -------------------- */}
-							<LinkContainer to="/login">
-								<Nav.Link>
-									<i className="fas fa-user"></i>Sign In
-								</Nav.Link>
-							</LinkContainer>
+							{/* -------------------- Profile Dropdown & login -------------------- */}
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id="username">
+									<LinkContainer to="/profile">
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to="/login">
+									<Nav.Link>
+										<i className="fas fa-user"></i>Sign In
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
@@ -34,4 +57,4 @@ const Header = () => {
 	);
 };
 
-export default Header;
+export default withRouter(Header);
