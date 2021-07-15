6fbb5eafdf0640c2c538317511b7de459a571fe4
zongxilli <zongxi2014@gmail.com>
2021-07-15 18:05:57 -0400

    add PUT request to update user profile

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -33,22 +33,22 @@ const authUser = expressAsyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = expressAsyncHandler(async (req, res) => {
-	const { name ,email, password } = req.body;
+	const { name, email, password } = req.body;
 
 	// To see if this user is already registered
 	const userExists = await User.findOne({ email: email });
 
 	// If this user is already registered
 	if (userExists) {
-		res.status(400)
-		throw new Error('User already exists')
+		res.status(400);
+		throw new Error('User already exists');
 	}
 
 	const user = await User.create({
 		name,
 		email,
-		password
-	})
+		password,
+	});
 
 	// This is a new user
 	if (user) {
@@ -58,10 +58,10 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 			email: user.email,
 			isAdmin: user.isAdmin,
 			token: generateToken(user._id),
-		})
-	}else {
-		res.status(404)
-		throw new Error('User not found')
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
 	}
 });
 
@@ -69,7 +69,6 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = expressAsyncHandler(async (req, res) => {
-
 	// Find user by users id
 	const user = await User.findById(req.user._id);
 
@@ -89,4 +88,36 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, registerUser, getUserProfile };
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+	// Find user by users id
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	}
+	// User does not exist => set status to 401 error & throw a error message
+	else {
+		res.status(404);
+		throw new Error('User not found');
+	}
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(notFound);
 // format all of the error message
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(
 	PORT,
