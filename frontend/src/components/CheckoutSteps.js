fe3631274985db505a2d5de358f7c8e0df7673df
zongxilli <zongxi2014@gmail.com>
2021-07-19 22:46:35 -0400

    Added shipping steps comnponents

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -16,7 +16,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
 			</Nav.Item>
 
 			<Nav.Item>
-				{step1 ? (
+				{step2 ? (
 					<LinkContainer to="/shipping">
 						<Nav.Link>Shipping</Nav.Link>
 					</LinkContainer>
@@ -26,7 +26,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
 			</Nav.Item>
 
 			<Nav.Item>
-				{step1 ? (
+				{step3 ? (
 					<LinkContainer to="/payment">
 						<Nav.Link>Payment</Nav.Link>
 					</LinkContainer>
@@ -36,7 +36,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
 			</Nav.Item>
 
 			<Nav.Item>
-				{step1 ? (
+				{step4 ? (
 					<LinkContainer to="/placeorder">
 						<Nav.Link>Place Order</Nav.Link>
 					</LinkContainer>
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 import { saveShippingAddress } from '../actions/cartActions';
 
 const ShippingScreen = ({ history }) => {
@@ -23,8 +24,10 @@ const ShippingScreen = ({ history }) => {
 
 	return (
 		<FormContainer>
-			<h1>Shipping</h1>
 			{/* //=-------------------- Shipping Form -------------------- */}
+			{/* .......... Shipping Steps .......... */}
+			<CheckoutSteps step1 step2 />
+			<h1>Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				{/* .......... Address .......... */}
 				<Form.Group controlId="address">
