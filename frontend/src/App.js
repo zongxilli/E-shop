c247f43e4a75f9be0bc862369be475a8471aaf18
zongxilli <zongxi2014@gmail.com>
2021-08-01 22:38:29 -0400

    initialized productEditScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,10 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
-import UserListScreen from './screens/UserListScreen'
-import UserEditScreen from './screens/UserEditScreen'
-import ProductListScreen from './screens/ProductListScreen'
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
 	return (
@@ -36,6 +37,7 @@ const App = () => {
 					<Route path="/admin/userlist" component={UserListScreen} />
 					<Route path="/admin/user/:id/edit" component={UserEditScreen} />
 					<Route path="/admin/productlist" component={ProductListScreen} />
+					<Route path="/admin/product/:id/edit" component={ProductEditScreen} />
 					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
